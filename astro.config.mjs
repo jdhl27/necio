ad5a82cb06d5083001dc1a37e94e5cb1353ada1a
jdhl27 <independientemedellin.27.03@gmail.com>
2024-08-13 00:43:13 -0500

    nuevo portafolio

---
+++ astro.config.mjs
@@ -0,0 +1,11 @@
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import netlify from "@astrojs/netlify";
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [sitemap()],
+  site: "https://astro-theme-creek.netlify.app/",
+  output: "server",
+  adapter: netlify()
+});
